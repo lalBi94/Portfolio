b09db000b33f86780912f20470e573484577e69a
Bilal <bilal@boudjemline.fr>
2024-05-30 02:32:51 +0200

    $

--- src/sections/Localisation.jsx
+++ src/sections/Localisation.jsx
@@ -7,14 +7,24 @@ export default function Localisation() {
                 Localisation
             </Typography>
             <Box component="div" display="flex" flexDirection="row" gap={2}>
-                <iframe
-                    width="200"
-                    height="200"
-                    id="gmap_canvas"
-                    src="https://maps.google.com/maps?width=200&amp;height=200&amp;hl=en&amp;q=%20Cambrai+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
-                ></iframe>
+                <Box>
+                    <iframe
+                        width="200"
+                        height="200"
+                        style={{
+                            border: "1px solid #777",
+                            borderRadius: "20px",
+                        }}
+                        id="gmap_canvas"
+                        src="https://maps.google.com/maps?width=200&amp;height=200&amp;hl=en&amp;q=%20Cambrai+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
+                    ></iframe>
+                </Box>
 
                 <iframe
+                    style={{
+                        border: "1px solid #777",
+                        borderRadius: "20px",
+                    }}
                     width="200"
                     height="200"
                     id="gmap_canvas"
